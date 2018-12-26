7aa2713cdf2b56d7d806f873391594c14d20c32e
Roy Wang <roy.wang@systemcom.com.tw>
2018-12-27 00:32:05 +0800

    增加context binding test

---
+++ src/ContextBindingTest/frmMain.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace ContextBindingTest
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.database2DataSet = new ContextBindingTest.Database2DataSet();
+            this.MyData_BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.MyData_TableAdapter = new ContextBindingTest.Database2DataSetTableAdapters.MY_DATATableAdapter();
+            this.識別碼DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.uSERNAMEDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pHONEDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.aGEDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mARRIEDDataGridViewCheckBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.aDDRESSDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database2DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MyData_BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.MyData_BindingSource, "USERNAME", true));
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(95, 6);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 27);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "名稱";
+            // 
+            // textBox2
+            // 
+            this.textBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.MyData_BindingSource, "AGE", true));
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(95, 43);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(132, 27);
+            this.textBox2.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 46);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "年齡";
+            // 
+            // textBox3
+            // 
+            this.textBox3.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.MyData_BindingSource, "PHONE", true));
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(369, 6);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(132, 27);
+            this.textBox3.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(287, 9);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 19);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "電話";
+            // 
+            // textBox4
+            // 
+            this.textBox4.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.MyData_BindingSource, "ADDRESS", true));
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(95, 80);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox4.MaxLength = 255;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(564, 27);
+            this.textBox4.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 83);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 19);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "地址";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.DataBindings.Add(new System.Windows.Forms.Binding("Checked", this.MyData_BindingSource, "MARRIED", true));
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Location = new System.Drawing.Point(291, 45);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(60, 23);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "已婚";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.識別碼DataGridViewTextBoxColumn,
+            this.uSERNAMEDataGridViewTextBoxColumn,
+            this.pHONEDataGridViewTextBoxColumn,
+            this.aGEDataGridViewTextBoxColumn,
+            this.mARRIEDDataGridViewCheckBoxColumn,
+            this.aDDRESSDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.MyData_BindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(17, 115);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(918, 283);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // database2DataSet
+            // 
+            this.database2DataSet.DataSetName = "Database2DataSet";
+            this.database2DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // MyData_BindingSource
+            // 
+            this.MyData_BindingSource.DataMember = "MY_DATA";
+            this.MyData_BindingSource.DataSource = this.database2DataSet;
+            // 
+            // MyData_TableAdapter
+            // 
+            this.MyData_TableAdapter.ClearBeforeFill = true;
+            // 
+            // 識別碼DataGridViewTextBoxColumn
+            // 
+            this.識別碼DataGridViewTextBoxColumn.DataPropertyName = "識別碼";
+            this.識別碼DataGridViewTextBoxColumn.HeaderText = "識別碼";
+            this.識別碼DataGridViewTextBoxColumn.Name = "識別碼DataGridViewTextBoxColumn";
+            // 
+            // uSERNAMEDataGridViewTextBoxColumn
+            // 
+            this.uSERNAMEDataGridViewTextBoxColumn.DataPropertyName = "USERNAME";
+            this.uSERNAMEDataGridViewTextBoxColumn.HeaderText = "USERNAME";
+            this.uSERNAMEDataGridViewTextBoxColumn.Name = "uSERNAMEDataGridViewTextBoxColumn";
+            // 
+            // pHONEDataGridViewTextBoxColumn
+            // 
+            this.pHONEDataGridViewTextBoxColumn.DataPropertyName = "PHONE";
+            this.pHONEDataGridViewTextBoxColumn.HeaderText = "PHONE";
+            this.pHONEDataGridViewTextBoxColumn.Name = "pHONEDataGridViewTextBoxColumn";
+            // 
+            // aGEDataGridViewTextBoxColumn
+            // 
+            this.aGEDataGridViewTextBoxColumn.DataPropertyName = "AGE";
+            this.aGEDataGridViewTextBoxColumn.HeaderText = "AGE";
+            this.aGEDataGridViewTextBoxColumn.Name = "aGEDataGridViewTextBoxColumn";
+            // 
+            // mARRIEDDataGridViewCheckBoxColumn
+            // 
+            this.mARRIEDDataGridViewCheckBoxColumn.DataPropertyName = "MARRIED";
+            this.mARRIEDDataGridViewCheckBoxColumn.HeaderText = "MARRIED";
+            this.mARRIEDDataGridViewCheckBoxColumn.Name = "mARRIEDDataGridViewCheckBoxColumn";
+            // 
+            // aDDRESSDataGridViewTextBoxColumn
+            // 
+            this.aDDRESSDataGridViewTextBoxColumn.DataPropertyName = "ADDRESS";
+            this.aDDRESSDataGridViewTextBoxColumn.HeaderText = "ADDRESS";
+            this.aDDRESSDataGridViewTextBoxColumn.Name = "aDDRESSDataGridViewTextBoxColumn";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(641, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 31);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "新增";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(741, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 31);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "修改";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(841, 6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 31);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "刪除";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(741, 43);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 31);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "儲存";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(841, 43);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(94, 31);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "放棄";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(947, 410);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmMain";
+            this.Text = "ContextBindingTest";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database2DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MyData_BindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private Database2DataSet database2DataSet;
+        private System.Windows.Forms.BindingSource MyData_BindingSource;
+        private Database2DataSetTableAdapters.MY_DATATableAdapter MyData_TableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 識別碼DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn uSERNAMEDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn pHONEDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn aGEDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn mARRIEDDataGridViewCheckBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn aDDRESSDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ src/ContextBindingTest/frmMain.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContextBindingTest
+{
+    public partial class frmMain : Form
+    {
+        private string MyConnStr = "";
+        private static readonly string ExePath = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+
+        private object NewObj = null;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void InitDB()
+        {
+            MyConnStr = string.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0}\\{1}",
+                            ExePath, "Database2.accdb");
+            
+            MyData_TableAdapter.Connection.ConnectionString = MyConnStr;
+        }
+
+        private void BindContext()
+        { }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            InitDB();
+
+            this.MyData_TableAdapter.Fill(this.database2DataSet.MY_DATA);
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex > 0)
+            {
+                MyData_BindingSource.Position = e.RowIndex;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MyData_BindingSource.AddNew();
+
+            textBox1.Enabled = textBox2.Enabled = textBox3.Enabled = textBox4.Enabled = checkBox1.Enabled = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MyData_BindingSource.EndEdit();
+            //endedit後 dataset.Table 就會更新，要把更新後的東西sync回DB
+            this.MyData_TableAdapter.Update(this.database2DataSet.MY_DATA);
+            this.MyData_TableAdapter.Fill(this.database2DataSet.MY_DATA);
+            textBox1.Enabled = textBox2.Enabled = textBox3.Enabled = textBox4.Enabled = checkBox1.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (MyData_BindingSource.Position >= 0)
+            {
+                textBox1.Enabled = textBox2.Enabled = textBox3.Enabled = textBox4.Enabled = checkBox1.Enabled = true;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MyData_BindingSource.CancelEdit();
+            this.MyData_TableAdapter.Update(this.database2DataSet.MY_DATA);
+            this.MyData_TableAdapter.Fill(this.database2DataSet.MY_DATA);
+            textBox1.Enabled = textBox2.Enabled = textBox3.Enabled = textBox4.Enabled = checkBox1.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var rows = dataGridView1.SelectedRows;
+
+            foreach (DataGridViewRow row in rows)
+            {
+                dataGridView1.Rows.Remove(row);
+            }
+
+            this.MyData_TableAdapter.Update(this.database2DataSet.MY_DATA);
+            this.MyData_TableAdapter.Fill(this.database2DataSet.MY_DATA);
+        }
+    }
+}
