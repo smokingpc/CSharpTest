cfaca698d579a04f541763a0b963f07ec1af10af
Roy Wang <roy.wang@systemcom.com.tw>
2019-01-23 01:26:02 +0800

    新增 InputValidateTest

---
+++ src/InputValidateTest/frmMain.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace InputValidateTest
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.button1 = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.dxErrorProvider1 = new DevExpress.XtraEditors.DXErrorProvider.DXErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dxErrorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(117, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(169, 27);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Validating += new System.ComponentModel.CancelEventHandler(this.textBox1_Validating);
+            this.textBox1.Validated += new System.EventHandler(this.textBox1_Validated);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Phone";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(117, 75);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(169, 27);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Validating += new System.ComponentModel.CancelEventHandler(this.textBox2_Validating);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 19);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Age";
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Location = new System.Drawing.Point(117, 43);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Properties.Appearance.Font = new System.Drawing.Font("Calibri", 12F);
+            this.textEdit1.Properties.Appearance.Options.UseFont = true;
+            this.textEdit1.Properties.Mask.BeepOnError = true;
+            this.textEdit1.Properties.Mask.EditMask = "0000-000000";
+            this.textEdit1.Properties.Mask.MaskType = DevExpress.XtraEditors.Mask.MaskType.Simple;
+            this.textEdit1.Properties.Mask.SaveLiteral = false;
+            this.textEdit1.Size = new System.Drawing.Size(169, 26);
+            this.textEdit1.TabIndex = 1;
+            this.textEdit1.Validating += new System.ComponentModel.CancelEventHandler(this.textEdit1_Validating);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(321, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 32);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "TEST";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // dxErrorProvider1
+            // 
+            this.dxErrorProvider1.ContainerControl = this;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 114);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textEdit1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmMain";
+            this.Text = "Input Validate Test";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dxErrorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private DevExpress.XtraEditors.DXErrorProvider.DXErrorProvider dxErrorProvider1;
+    }
+}
+
---
+++ src/InputValidateTest/frmMain.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InputValidateTest
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            dxErrorProvider1.SetErrorType();
+        }
+
+        private void textBox1_Validating(object sender, CancelEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("textBox1_Validating()");
+            // Validation 是在 LostFocus前先檢查整個text是否合乎格式
+            if ("" == textBox1.Text)
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(textBox1, "Please input your name!!");
+            }
+        }
+
+        private void textEdit1_Validating(object sender, CancelEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("textEdit1_Validating()");
+
+            //TextEdit的 Mask 是在 keydown 就作用，過濾掉不合規範的字元
+            //但 Validation 是在 LostFocus前先檢查整個text是否合乎格式
+            // 例如 電話欄位  區域號碼是數字，但沒有 1234 這種區域號碼
+            // 這就要在 Validation 去檢查
+
+            //Validation 分為 Validating / Validated 兩個event
+            //一個是 pre-event , 一個是 post-event
+            if ("" == textEdit1.Text)
+            {
+                e.Cancel = true;
+                dxErrorProvider1.SetError(textBox1, "Please input your phone number!!");
+            }
+            else
+            { 
+                
+            }
+        }
+
+        private void textBox2_Validating(object sender, CancelEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("textBox2_Validating()");
+            // Validation 是在 LostFocus前先檢查整個text是否合乎格式
+            try
+            {
+                int age = int.Parse(textBox2.Text);
+            }
+            catch (Exception ex)
+            {
+                errorProvider1.SetError(textBox2, "Please input correct Age!!");
+                e.Cancel = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("button1_Click()");
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("frmMain_FormClosing()");
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x10)
+            {
+                textBox1.CausesValidation = textBox2.CausesValidation = textEdit1.CausesValidation = false;
+                System.Diagnostics.Debug.WriteLine("Disable CauseValidation in all input.");
+            }
+            base.WndProc(ref m);
+        }
+
+        private void textBox1_Validated(object sender, EventArgs e)
+        {
+            errorProvider1.SetError(textBox1, "");
+        }
+    }
+}
