9ff78d0b026c1c3be732cb9d4b47aaca724b0c91
Roy Wang <roy.wang@systemcom.com.tw>
2019-08-31 18:55:33 +0800

    增加NLogTest實驗

---
+++ src/NLogTest/frmMain.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace NLogTest
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 16);
+            this.button2.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(250, 50);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Write 1 line to Log";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(15, 80);
+            this.button3.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(250, 50);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Write 1000000 lines Log";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 140);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "frmMain";
+            this.Text = "example to use NLog";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ src/NLogTest/frmMain.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NLogTest
+{
+    public partial class frmMain : Form
+    {
+        //init NLog
+        private NLog.Logger Log1 = NLog.LogManager.GetLogger("mytest1");
+        private NLog.Logger Log2 = NLog.LogManager.GetLogger("abc");
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string msg = "Who is calling a fleet ?";
+            Log1.Error(msg + "1");
+            Log2.Error(msg + "2");
+
+            MessageBox.Show("Done!");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("Done!");
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
