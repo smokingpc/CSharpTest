b120ba8e3543c7cb38880ec1c92c43fd249d5621
Roy Wang <roy.wang@systemcom.com.tw>
2020-03-07 00:39:03 +0800

    增加ComboBox databinding實驗

---
+++ src/ComboBoxTest/frmMain.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ComboBoxTest
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(174, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Show Binded Data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(52, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(239, 27);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 103);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 27);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(130, 103);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 27);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(248, 103);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 27);
+            this.textBox3.TabIndex = 2;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 146);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ComboBox Test";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ src/ComboBoxTest/frmMain.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComboBoxTest
+{
+    public partial class frmMain : Form
+    {
+        public CBindData MyBindData = new CBindData();
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        private void SetupComboBox()
+        {
+            List<CMyData> list = new List<CMyData>();
+            list.Add(new CMyData(1,"Kevin", "Addr1"));
+            list.Add(new CMyData(3, "Roy", "2addr"));
+            BindingList<CMyData> src = new BindingList<CMyData>(list);
+            comboBox1.DataSource = src;
+            comboBox1.DisplayMember = "Name";
+            comboBox1.ValueMember = "SN";
+        }
+
+        private void SetupBinding()
+        {
+            //one-way binding
+            var bind = new Binding("SelectedValue", MyBindData, "SN")
+                {
+                    DataSourceUpdateMode = DataSourceUpdateMode.OnPropertyChanged,
+                    ControlUpdateMode = ControlUpdateMode.Never,        //don't update control
+                };
+            comboBox1.DataBindings.Add(bind);
+
+            //one-way binding
+            bind = new Binding("Text", MyBindData, "Name")
+                {
+                    DataSourceUpdateMode = DataSourceUpdateMode.OnPropertyChanged,
+                    ControlUpdateMode = ControlUpdateMode.Never,        //don't update control
+                };
+            comboBox1.DataBindings.Add(bind);
+
+            //one-way binding
+            bind = new Binding("SelectedItem", MyBindData, "Data")
+            {
+                DataSourceUpdateMode = DataSourceUpdateMode.OnPropertyChanged,
+                ControlUpdateMode = ControlUpdateMode.Never,        //don't update control
+            };
+            comboBox1.DataBindings.Add(bind);
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            SetupComboBox();
+            SetupBinding();
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = MyBindData.SN.ToString();
+            textBox2.Text = MyBindData.Name;
+            textBox3.Text = MyBindData.Data.ToString();
+        }
+    }
+
+    public class CBindData
+    { 
+        public int SN { get; set; }
+        public string Name { get; set; }
+        public CMyData Data { get; set; }
+    }
+
+    public class CMyData
+    { 
+        public int SN { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public CMyData()
+        { }
+        public CMyData(int sn, string name, string addr)
+        {
+            this.SN = sn;
+            this.Name = name;
+            this.Address = addr;
+        }
+    }
+}
