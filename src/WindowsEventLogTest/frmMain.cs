b13804e7adcb8bfd29f3ce7f2744bb798c4942cd
Roy Wang - 王效鵬 <roy.wang@systemcom.com.tw>
2020-04-20 12:45:18 +0800

    增加 WindowsEventLogTest

---
+++ src/WindowsEventLogTest/frmMain.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsEventLogTest
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string source = textBox1.Text;
+            string logger = comboBox1.Text;
+            string msg = textBox3.Text;
+
+            if (!EventLog.SourceExists(source))
+            {
+                EventLog.CreateEventSource(source, logger);
+            }
+
+            var log = new EventLog(logger, ".", source);
+            
+            log.WriteEntry(msg, EventLogEntryType.Error, 1234, 5678);
+        }
+    }
+}
