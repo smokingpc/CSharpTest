1ef7b798523cc35888c355361a082fbb984c116c
Roy Wang - 王效鵬 <roy.wang@systemcom.com.tw>
2020-07-14 13:54:12 +0800

    增加 AppConfig 操作實驗

---
+++ src/AppConfigTest/frmMain.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppConfigTest
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.PerUserRoamingAndLocal);
+            var name = config.AppSettings.Settings["Name"];
+            var tel = config.AppSettings.Settings["Tel"];
+            var foods = config.AppSettings.Settings["Foods"];
+
+            if (null != name)
+                textBox1.Text = name.Value;
+
+            if (null != tel)
+                textBox2.Text = tel.Value;
+
+            var foodlist = foods.Value.Split(',');
+            textBox3.Text = foodlist[0];
+            textBox4.Text = foodlist[1];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            string tel = textBox2.Text;
+
+            //ConfigurationUserLevel.None will open "AppConfigTest.exe.Config" file in exe path.
+            //  ConfigurationUserLevel.PerUserRoamingAndLocal and  ConfigurationUserLevel.PerUserRoaming
+            //  will open roaming config in user profile path.
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var settings = config.AppSettings.Settings;
+            settings["Name"].Value = name;  //update value
+            //settings.Add("Name", name);   //add value
+
+            settings["Tel"].Value = tel;
+            //settings.Add("Tel", tel);
+
+            string data = textBox3.Text + "," + textBox4.Text;
+            settings["Foods"].Value = data;
+            //settings.Add("Foods", data);
+            config.Save(ConfigurationSaveMode.Modified);
+        }
+    }
+}
