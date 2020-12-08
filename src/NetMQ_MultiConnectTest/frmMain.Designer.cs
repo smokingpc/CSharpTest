da4a3212eb61d4981a4c2f94a2543972612618a8
Roy Wang - 王效鵬 <roy.wang@systemcom.com.tw>
2020-12-08 15:31:50 +0800

    add NetMQ multiple connect test

---
+++ src/NetMQ_MultiConnectTest/frmMain.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace NetMQ_MultiConnectTest
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Calibri", 16F);
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Console";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 96);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(893, 516);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(917, 624);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NetMQ MultiConnect Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ src/NetMQ_MultiConnectTest/frmMain.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace NetMQ_MultiConnectTest
+{
+    public partial class frmMain : Form
+    {
+        bool Running = false;
+        int WaitInterval = 20;
+        int SleepTime = 2000;
+        ManualResetEventSlim EventStop = new ManualResetEventSlim(false);
+        List<int> PortList = new List<int>() { 55688, 55689, 55690 };
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Running)
+            {
+                EventStop.Set();
+                button1.Text = "GO";
+            }
+            else
+            {
+                EventStop.Reset();
+                Task.Run(ThreadPub);
+                Task.Run(ThreadSub);
+                button1.Text = "STOP";
+            }
+            Running ^= true;
+        }
+
+        private void ThreadPub()
+        {
+            List<PublisherSocket> socklist = new List<PublisherSocket>();
+            foreach (var port in PortList)
+            {
+                string addr = $"tcp://{IPAddress.Any}:{port}";
+                var sock = new PublisherSocket();
+                sock.Options.IPv4Only = true;
+                sock.Bind(addr);
+                socklist.Add(sock);
+            }
+
+            while (EventStop.Wait(SleepTime) == false)
+            {
+                DateTime timestamp = DateTime.Now;
+                for (int i = 0; i < socklist.Count; i++)
+                {
+                    string prefix = "";
+                    switch (i)
+                    {
+                        case 0:
+                            prefix = "I am first!";
+                            break;
+                        case 1:
+                            prefix = " second one";
+                            break;
+                        case 2:
+                            prefix = "I want to go home";
+                            break;
+                    }
+                    string data = $"[from {prefix}] timestamp={timestamp.ToString("HH:mm:ss.fff")}";
+                    var buffer = Encoding.Unicode.GetBytes(data);
+                    var sock = socklist[i];
+                    sock.SendFrame(buffer);
+                }
+            }
+
+            foreach (var sock in socklist)
+            {
+                sock.Close();
+            }
+        }
+
+        private void ThreadSub()
+        {
+            List<SubscriberSocket> socklist = new List<SubscriberSocket>();
+            var sock = new SubscriberSocket();
+            sock.Options.IPv4Only = true;
+            sock.SubscribeToAnyTopic();
+
+            foreach (var port in PortList)
+            {
+                string addr = $"tcp://{IPAddress.Loopback}:{port}";
+                sock.Connect(addr);
+            }
+
+            Thread.Sleep(2000);
+
+            while (EventStop.Wait(WaitInterval) == false)
+            {
+                var buffer = sock.ReceiveFrameBytes();
+                string msg = Encoding.Unicode.GetString(buffer);
+                textBox1.AppendLine(msg);
+            }
+
+            sock.Close();
+        }
+    }
+
+    public static class Extension
+    {
+        public static void AppendLine(this TextBox tb, string msg)
+        {
+            if (tb.InvokeRequired)
+            {
+                tb.BeginInvoke((Action)(() => { AppendLine(tb, msg); }));
+            }
+            else
+            {
+                tb.AppendText(msg + "\r\n");
+            }
+        }
+    }
+}
