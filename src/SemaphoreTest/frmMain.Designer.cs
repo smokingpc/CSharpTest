059e26b5c39a7bc518fe07050864d7191b6baa8d
Roy Wang - 王效鵬 <roy.wang@systemcom.com.tw>
2020-12-22 15:29:00 +0800

    add Semaphore Test

---
+++ src/SemaphoreTest/frmMain.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace SemaphoreTest
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thread Count";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 19);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Max Semaphore";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(147, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 27);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "10";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(147, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 27);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "5";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(8, 105);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.Size = new System.Drawing.Size(605, 209);
+            this.textBox3.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 19);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "LOG";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(304, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 326);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Semaphore Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ src/SemaphoreTest/frmMain.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SemaphoreTest
+{
+    public partial class frmMain : Form
+    {
+        private int ThreadCount = 0;
+        private int WaitCount = 0;
+        private int LockCount = 0;
+        private Semaphore SemaphoreLock = null;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int count = int.Parse(textBox1.Text);
+            ThreadCount = WaitCount = LockCount = 0;
+            int max_semaphore = int.Parse(textBox2.Text);
+            SemaphoreLock = new Semaphore(max_semaphore, max_semaphore, "MyTestSemaphore");
+
+            for (int i = 0; i < count; i++)
+                Task.Run(DoJob);
+        }
+
+        private void DoJob()
+        {
+            int count1 = Interlocked.Increment(ref ThreadCount);
+            int count2 = Interlocked.Increment(ref WaitCount);
+            textBox3.AppendLine($"[EnterThread] Thread Count ={count1}, WaitCount={count2}");
+
+            //Accquire Semaphore. If max count reached , this method will block until
+            //semaphore count < max semaphore.
+            SemaphoreLock.WaitOne();
+
+            count2 = Interlocked.Decrement(ref WaitCount);
+            int count3 = Interlocked.Increment(ref LockCount);
+            textBox3.AppendLine($"[EnterSemaphore] Wait Count={count2}, Locked Count ={count3}");
+            Thread.Sleep(3000);
+
+            //Release accquired Semaphore.
+            SemaphoreLock.Release();
+            count1 = Interlocked.Decrement(ref ThreadCount);
+            count3 = Interlocked.Decrement(ref LockCount);
+            textBox3.AppendLine($"[LeaveThread] Thread Count ={count1}, Locked Count ={count3}");
+        }
+    }
+
+    public static class Extension 
+    {
+        public static void AppendLine(this TextBox tb, string msg)
+        {
+            if (tb.InvokeRequired)
+            {
+                tb.Invoke((Action)(() => { tb.AppendLine(msg); }));
+            }
+            else
+            {
+                tb.AppendText(msg + "\r\n");
+            }
+        }
+    }
+}
