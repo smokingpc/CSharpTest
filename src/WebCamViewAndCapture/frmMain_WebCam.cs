43da215db1be823503dd07172fdf4d896b330b91
SmokingPC <smokingpc@gmail.com>
2022-04-28 21:51:04 +0800

    add webcam test

---
+++ src/WebCamViewAndCapture/frmMain_WebCam.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Windows.Media.Capture;
+using Windows.Storage;
+
+
+namespace WebCamViewAndCapture
+{
+    public partial class frmMain : Form
+    {
+    }
+}
